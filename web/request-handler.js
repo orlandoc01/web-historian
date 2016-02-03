1819d4c3162b1ecf529511588b6a18087cc0d4bf
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-03 14:46:32 -0800

    Implement GET request for root path

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+//requests to '/'
+var rootActions = {
+  'GET': function(req, resp) {
+    fs.readFile(path.join(archive.paths.siteAssets,"/index.html"),
+     (err, data) => {
+      if(err) {
+        console.error(err);
+      }
+      httpHelpers.sendResponse(resp, data);
+     });
+  },
+
+  'POST': function() {},
+
 };
+            
+exports.handleRootRequest = httpHelpers.makeHandler(rootActions);
