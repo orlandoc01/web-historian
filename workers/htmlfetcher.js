6653f3d76185a98863a3bb3513ba18b055bc4386
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-03 19:55:41 -0800

    Implement worker htmlfetcher script

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,14 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var http = require('http');
+var archive = require('archive-helpers');
+
+archive.readListOfUrls( function(urls) {
+  urls.forEach( function(url) {
+    archive.isUrlArchived(url, function(found) {
+      if(!found) {
+        archive.downloadUrls([url]);
+      }
+    });
+  });
+});
